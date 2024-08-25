b4102e46b0986dcc25088feb2a8ea100d5e8f464
saad-nadeem <saadnadeem439922@gmail.com>
2024-08-25 23:42:11 +0500

    undate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,37 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
-const page = () => {
-  return(
-    <dev><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-    <img className="w-24 h-24 md:rounde rounded-full mx-auto" src="/saad.a.avif" alt="" width="384" height="512"/>
-    <div className="pt-6 md:p-8 text-center space-y-4">
-      <blockquote>
-        <p className="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-        </p>
-      </blockquote>
-      <figcaption className="font-medium">
-        <div className="text-sky-500 dark:text-sky-400">
-        Sarah Dayan 
+const Page = () => {
+  return (
+    <div>
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <Image
+          className="w-24 h-24 md:rounded-full mx-auto"
+          src="/saad.a.avif"
+          alt="Profile picture of Sarah Dayan"
+          width={384}
+          height={512}
+        />
+        <div className="pt-6 md:p-8 text-center space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I&apos;ve seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+              Sarah Dayan
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
         </div>
-        <div className="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-        </div>
-      </figcaption>
+      </figure>
     </div>
-  </figure></dev>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
